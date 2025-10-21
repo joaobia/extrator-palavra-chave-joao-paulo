2b7c16bca51806955b3ae02403f50e8ba0b40a92
joaobia <heinzen.joao@escola.pr.gov.br>
2025-10-21 09:24:59 -0300

    21/10

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const botaoMotraPlavras = document.querySelector("#botao-palavrachave;");
 botaoMotraPlavras.addEventListener("click",mostraPalavrasChave);
 
 function mostraPalavrasChave(){
-    alert("fui clicado!");
+    alert("Fui clicado!");
 }
