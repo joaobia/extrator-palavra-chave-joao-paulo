db376f5cead6639187fb82f79cc73fd880adb8e5
joaobia <heinzen.joao@escola.pr.gov.br>
2025-10-21 10:38:02 -0300

    21/10

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const botaoMostraPlavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPlavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){
+function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavraChave");
     const palavras = texto.split(" ");
