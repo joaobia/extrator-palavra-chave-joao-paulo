2f50924178ff1aa07c2654d0b3ad4f53d07bfdbf
joaobia <heinzen.joao@escola.pr.gov.br>
2025-10-28 10:45:10 -0300

    28/10

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ campoResultado.textContent = palavrasChave.join(", ")
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    
+function ordenaPalavra(p1, p2){
+    return frequencias [p2] - frequencias[p1];
+}
+return ordenadas.slice(0, 10);
+}
+
+
+        function contaFrequencias(palavras){
+
     let frequencias = {};
 
     for (let i of palavras) {
@@ -26,4 +38,7 @@ function processaTexto(texto) {
     }
 
     return palavras;
-}
+
+
+
+        }
