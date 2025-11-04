bcc50bfc35afe74bca5b1052e3c432f25cde2c2b
joaobia <heinzen.joao@escola.pr.gov.br>
2025-11-04 10:21:00 -0300

    04/11/2025

--- script.js
+++ script.js
@@ -47,3 +47,15 @@ return ordenadas.slice(0, 10);
 
 
         }
+
+        function tiraPalavrasRuins(palavras){
+            const PALAVRAS_RUINS = new Set(["para", "nos", "das", "como", "que"]);
+            const palavrasBoas = [];
+
+            for (let palavra of palavras){
+                if (!PALAVRAS_RUINS.has(palavra)&& palavra.length > 2){
+                    palavrasBoas.push(palavra);
+                }
+            }
+return palavrasBoas;
+        }
